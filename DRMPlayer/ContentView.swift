259a7017dceb949056fabf43eb09ac5ab88528ca
Samuel Défago <defagos@gmail.com>
2025-07-26 10:57:37 +0200

    Create project

---
+++ DRMPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DRMPlayer
+//
+//  Created by Samuel Défago on 26.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DRMPlayer/DRMPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DRMPlayerApp.swift
+//  DRMPlayer
+//
+//  Created by Samuel Défago on 26.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct DRMPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
